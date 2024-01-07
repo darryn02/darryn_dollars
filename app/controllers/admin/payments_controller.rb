79caf7290a93bfee5b11f585d18cccc69f923d44
Darryn <darryn02@users.noreply.github.com>
2024-01-06 19:40:34 -0500

    Fix payment submission

--- app/controllers/admin/payments_controller.rb
+++ app/controllers/admin/payments_controller.rb
@@ -1,5 +1,7 @@
 module Admin
   class PaymentsController < ApplicationController
+    before_action :sanitize_params
+
     def create
       Payment.create!(payment_params.reverse_merge(submitted_at: Time.now))
       redirect_to admin_dashboard_path
@@ -8,11 +10,11 @@ module Admin
     private
 
     def payment_params
-      params.require(:payment).permit(:account_id, :amount, :submitted_at).sanitize_params
+      params.require(:payment).permit(:account_id, :amount, :submitted_at)
     end
 
     def sanitize_params
-      params[:wager][:amount] = params[:wager][:amount].gsub(/[\$\,]/, "")
+      params[:payment][:amount] = params[:payment][:amount].gsub(/[\$\,]/, "")
     end
   end
 end
